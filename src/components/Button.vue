faa3f73ec876aa70addb72c9f7e6862eb203157d
DmitriyKaplan28 <dsk007@mail.ru>
2023-01-25 01:04:13 +0300

    feat: initial css

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="button-container">
+    <button class="button">Was clicked:</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonOne',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.button-container{
+  display: flex;
+  justify-content: center;
+}
+
+.button {
+  width: 160px;
+  height: 50px;
+  border-radius: 10px;
+  border: none;
+  background-color: blue;
+  color: white;
+}
+</style>
